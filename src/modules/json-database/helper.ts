d6e409ab9d91a171fcf1c451be9a368b4de0c925
Mirasaki <pcr.hillebrand1998@gmail.com>
2024-06-08 18:52:42 +0200

    feat: improved async file operation error handling

--- src/modules/json-database/helper.ts
+++ src/modules/json-database/helper.ts
@@ -1,40 +1,42 @@
-import { readFileSync, writeFileSync, rmSync } from 'fs';
 import { getFiles } from '../files';
 import { debugLog } from '../../debug';
+import { rm, writeFile, readFile } from 'fs/promises';
 
-export const getAllJSONFiles = (targetPath: string) => {
+export const getAllJSONFiles = async (targetPath: string) => {
   const allJSONFiles = getFiles(targetPath, [ '.json' ]);
   debugLog(`Found ${allJSONFiles.length} JSON files in ${targetPath}`);
-  return allJSONFiles
-    .map((pathToJSON) => {
-      const textData = readFileSync(pathToJSON, { encoding: 'utf-8' });
+  return (await Promise.all(
+    allJSONFiles.map(async (pathToJSON) => {
+      const textData = await readFile(pathToJSON, { encoding: 'utf-8' });
       try {
         return JSON.parse(textData);
       }
-      catch {
+      catch (err) {
+        console.error(`Error parsing JSON file at ${pathToJSON}`, err);
         // Either invalid syntax in JSON or not a file
         // full path is shown in those errors, avoid
         return null;
       }
     })
-    .filter((e) => !!e); // Keep only truthy
+  )).filter((e) => !!e); // Keep only truthy
 };
 
-export const getJSONFile = (targetPath: string, identifier: string) => {
+export const getJSONFile = async (targetPath: string, identifier: string) => {
   const finalPath = targetPath + `/${ identifier }.json`;
   debugLog(`Attempting to get JSON file from ${finalPath}`);
   try {
-    const textData = readFileSync(finalPath, { encoding: 'utf-8' });
+    const textData = await readFile(finalPath, { encoding: 'utf-8' });
     return JSON.parse(textData);
   }
   catch (err) {
+    console.error(`Error reading JSON file from ${finalPath}`, err);
     // Either invalid syntax in JSON or not a file
     // full path is shown in those errors, avoid
     return null;
   }
 };
 
-export const putJSONFile = (
+export const putJSONFile = async (
   targetPath: string,
   identifier: string,
   newJSONContent: unknown
@@ -42,26 +44,29 @@ export const putJSONFile = (
   const finalPath = targetPath + `/${ identifier }.json`;
   debugLog(`Attempting to put JSON file to ${finalPath}`);
   try {
-    writeFileSync(
+    await writeFile(
       finalPath,
       JSON.stringify(newJSONContent, null, 2),
-      { encoding: 'utf-8' }
+      { encoding: 'utf-8'}
     );
     return true;
   }
-  catch {
+  catch (err) {
+    console.error(`Error writing JSON file to ${finalPath}`, err);
     // Either invalid syntax in JSON or not a file
     // full path is shown in those errors, avoid
     return null;
   }
 };
 
-export const deleteJSONFile = (targetPath: string, identifier: string) => {
+export const deleteJSONFile = async (targetPath: string, identifier: string) => {
   const finalPath = targetPath + `/${ identifier }.json`;
+  debugLog(`Attempting to delete JSON file at ${finalPath}`);
   try {
-    rmSync(finalPath, { recursive: false, force: false });
+    await rm(finalPath, { recursive: false, force: false });
   }
   catch (err) {
+    console.error(`Error deleting JSON file at ${finalPath}`, err);
     // Either invalid syntax in JSON or not a file
     // full path is shown in those errors, avoid
     return null;
